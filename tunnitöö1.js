b55e05d40f750a850c01c3819f3b41429dff1c89
martenpip <89940567+martenpip@users.noreply.github.com>
2021-09-10 10:00:00 +0300

    temp

--- tunnitöö1.js
+++ tunnitöö1.js
@@ -9,24 +9,43 @@ let töötaja2 = {
 
 // ctrl + K   ctrl + C comment
 // ctrl + K   ctrl + U uncomment
-//Home + end = real algusesse
-//typeof special key word millega saab teada millene muutuja kuskil sõna sees on
+// Home + end = real algusesse
+// typeof special key word millega saab teada millene muutuja kuskil sõna sees on
 // primitive types: number string boolean
 // object function undefined
 // user defined types:
 // Math 
+// 
 
 
 function summa() {
     console.log (7+5)
 }
 
+let töötaja = {nimi: "Timo", palk: 10
+    nimi "Timo"
+    palk : 15,
+    Rahakott: 5, 
+    maksapalk: function (){
+    }
+}
+
+console.log(töötaja.nimi)
+töötaja ["palk"]
+töötaja.palk += 5
+
+töötaja.maksaPalk()
+
+let muuda = "palk"
+
 let summa = function
 
+//typeof summa
 
 typeof summa
 
 // skoop scope 
+
 let rahakott = 10; 
 let rahakott = 5;
 if (rahakott >= tootehind) {
@@ -37,4 +56,23 @@ if (rahakott >= tootehind) {
    *=
    /=
 }
-console.log(rahakott)
+
+
+function checkTemp () {
+    let temp = 10;
+
+    if(temp <= 20){
+        console.log("Liiga külm!")
+    };
+    if(temp >21 && temp >= 40){
+        console.log("Paras temperatuur.")
+    };
+    if(temp > 41){
+        console.log("Liiga kuum!")
+    };
+}
+
+
+
+
+
