5eef566571624db13930cade7637c50b08f7df24
martenpip <89940567+martenpip@users.noreply.github.com>
2021-09-10 08:40:12 +0300

    tunnitöö

---
+++ tunnitöö1.js
@@ -0,0 +1,18 @@
+// Object on tüüp mis salvesta key-value paare.
+// key: value
+let töötaja = {nimi: "Timo",palk: 10,}
+let töötaja2 = {
+    nimi: "Timo",
+    palk: 10,
+}
+
+let rahakott = 10; 
+let rahakott = 5;
+if (rahakott >= tootehind) {
+   // rahakott = rahakott - tootehind
+   rahakott -= tootehind
+   +=
+   *=
+   /=
+}
+console.log(rahakott)
