fd5a2d2d142db72202df0bf9437e76e11d16ded3
martenpip <89940567+martenpip@users.noreply.github.com>
2021-09-10 08:56:16 +0300

    function

--- tunnitöö1.js
+++ tunnitöö1.js
@@ -5,10 +5,20 @@ let töötaja2 = {
     nimi: "Timo",
     palk: 10,
 }
+// ctrl + K   ctrl + C comment
+// ctrl + K   ctrl + U uncomment
+//Home + end = real algusesse
 
+function summa() {
+    console.log (7+5)
+}
+
+
+// skoop scope 
 let rahakott = 10; 
 let rahakott = 5;
 if (rahakott >= tootehind) {
+    let rahakott = rahakott - tootehind
    // rahakott = rahakott - tootehind
    rahakott -= tootehind
    +=
