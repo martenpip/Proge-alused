524a35cadd419085dca4d2b96935c94127594f32
martenpip <89940567+martenpip@users.noreply.github.com>
2021-09-17 09:12:38 +0300

    kuu

---
+++ tunnitöö2.js
@@ -0,0 +1,24 @@
+// array []
+// object {}
+
+let töötaja = {
+    nimi: "Timo",
+    palk 10
+
+}
+
+
+          0    1   2   3   4   5   6
+let et = [10, 12, 15, 12, 13, 15, 11];
+console.log (et)
+console.log(et [tahaTeada])
+
+let tahaTeada = prompt("Valitud kuu!");
+let et = [1,5,7,3,3,4,1,2,9,4,9,5];
+console.log(et[tahaTeada]);
+
+let tahaTeada = prompt("Valitud kuu!");
+let et = [1,5,7,3,3,4,1,2,9,4,9,5];
+let kuu = ["Jaanuar","Veebruar","Marts","Aprill","Mai","Juuni","Juuli","August","September","Oktoober","November","Detsembr"]
+console.log(kuu[tahaTeada-1]+" tarbimine on "+et[tahaTeada-1]); 
+
