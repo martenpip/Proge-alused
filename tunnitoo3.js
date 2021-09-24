51395b85da6b1ed2d9de5daea303d1469b8c5770
martenpip <89940567+martenpip@users.noreply.github.com>
2021-09-24 10:10:55 +0300

    wtf

--- tunnitoo3.js
+++ tunnitoo3.js
@@ -1,10 +1,17 @@
-//Array CRUDE  
+//Array CRUDE (list, massiiv, array) 
 Create [] 
 ["test", 2, 3, null] 
 console.log([])
 let list = []
-let list 
+let list
+
 //Read 
+//Create
+//Update
+//Delete
+//Read
+
+list.length // mitu elementi listis on
 
 list [0] // "test"
 list [1] // 2
@@ -16,4 +23,24 @@ fruits.indefOf("pear") // -1
 fruits.indefOf("apple") // 2
 fruits.includes("pear") // false
 list.filter(elm => elm => 0) // 2,3
+
+let fruits = ["orange", "pear", "pineapple"]; fruits.indexOf("pear")
+1
+fruits.push("pear")
+4
+fruits.push("pear")
+5
+fruits.push("pear")
+6
+fruits.push("pear")
+7
+fruits.push("pear")
+8
+fruits.push("pear")
+9
+fruits.push("pear")
+fruits[4] = "watermelon"
+
+// IndexOf kus positsioonil element arrays on
+// -1 on et ei ole arrays
 
