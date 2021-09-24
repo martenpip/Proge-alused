4bc60b662cdc7bf298b907fcc501e59af67911ff
martenpip <89940567+martenpip@users.noreply.github.com>
2021-09-24 08:33:30 +0300

    ...

--- tunnitoo3.js
+++ tunnitoo3.js
@@ -1,6 +1,19 @@
-Array CRUDE  
+//Array CRUDE  
 Create [] 
 ["test", 2, 3, null] 
 console.log([])
 let list = []
-Read 
+let list 
+//Read 
+
+list [0] // "test"
+list [1] // 2
+list [2] // 3
+list [3] // null
+list [4] // undefined
+list [3] // null
+fruits.indefOf("pear") // -1
+fruits.indefOf("apple") // 2
+fruits.includes("pear") // false
+list.filter(elm => elm => 0) // 2,3
+
