c089d2e6123b8e9fbc214e9ce8c708c5d3cd7870
martenpip <89940567+martenpip@users.noreply.github.com>
2021-09-24 08:27:00 +0300

    hehe

---
+++ tunnitoo3.js
@@ -0,0 +1,6 @@
+Array CRUDE  
+Create [] 
+["test", 2, 3, null] 
+console.log([])
+let list = []
+Read 
--- tunnitöö2.js
+++ tunnitöö2.js
@@ -1,14 +1,7 @@
 // array []
 // object {}
 
-let töötaja = {
-    nimi: "Timo",
-    palk 10
-
-}
-
-
-          0    1   2   3   4   5   6
+//         0    1   2   3   4   5   6
 let et = [10, 12, 15, 12, 13, 15, 11];
 console.log (et)
 console.log(et [tahaTeada])
@@ -47,7 +40,10 @@ while(toota) {
 }
 
 const data = ["FWmcfgAWxe","KnFJXGcACq","cCYApSPiX0","iHwbq2giBx","X5Ebl6sjLf","Rxc2oFCgI4","jFtFONF7NK","b8iIhVOJl6","aGlxlytbic","30e8YSgLzd","0oj65bHiHZ","NtykZPC0DH","qgj6THRgT0","rFM7G7SJbB","lcu5UKrj7w","xurxryxn5u","7XDXFJ8Axh","RuQU6EeJyi","go3SD5CYYT","c8sOoXTa59","Vh0QfTkdKk","rrYBTg4sWc","CmeWiA73ig","iM8MD2IXle","SQk4WlTLiE","wn3JhYo93i","Hlh1Amt1jo","Rd6hiOekcE","LMKIBsBBmQ","nb72pL5LKQ","8GufbXPMRb","OO5OnLzsdP","hLddf4izXl","6x8suntNs6","8VATvVOQZR","jxL8zSOcZN","87YqnuB6Mc","sqr1WbVsWv","fLC17MbW59","tJ6Z5r6mDd","iiEffxACk7","C6w4k9AS2N","9rQTNM8xmG","x4JQLDr6hd","3bYXSm7Zgo","phTYCmMxOW","isl31Fd0SN","CZItvlfdqW"];
-for (x = 1; x < data.length; x++){
-    console.log(data[x])
+function lugemine() {
+    console.log(search('W'))
+   
 }
 
+//string.contains()
+//string.indexOf()
