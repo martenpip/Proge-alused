b7896c50c5410de9320acd5767609f342868632b
martenpip <89940567+martenpip@users.noreply.github.com>
2021-10-01 10:14:17 +0300

    mitu numbrit on suuremad kui 4 ja mitu on vaiksemad kui 4

--- tunnitoo5.js
+++ tunnitoo5.js
@@ -26,3 +26,21 @@ for (let i = 0; i < arr.length; i++) {
     else
       console.log(arr[i]+" on 4 või väiksem")
   }
+
+
+
+  let arr = [5,8,4,2,3,1];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > 4)
+    console.log(arr[i]+" on suurem kui 4")
+  else
+    console.log(arr[i]+" on 4 või väiksem")
+}
+
+let onSuuremKuiNeli = 0
+for(let i=0; i<arr.length; i++) {
+  if (arr[i] > 4)
+    onSuuremKuiNeli++
+}
+console.log(onSuuremKuiNeli + " numbrit on suurem kui 4")
+console.log((arr.length - onSuuremKuiNeli) + " numbrit on väiksemad kui 4")
