b899398cc8b086be3cee89d0ca5994de921ed265
martenpip <89940567+martenpip@users.noreply.github.com>
2021-10-01 09:26:27 +0300

    boop

--- tunnitoo3.js
+++ tunnitoo3.js
@@ -1,5 +1,5 @@
 //Array CRUDE (list, massiiv, array) 
-Create [] 
+//Create [] 
 ["test", 2, 3, null] 
 console.log([])
 let list = []
@@ -44,3 +44,16 @@ fruits[4] = "watermelon"
 // IndexOf kus positsioonil element arrays on
 // -1 on et ei ole arrays
 
+
+const offers=[["Crossroad Gardens","88"],["Strawberry Mountain Lands","57"],["Weeping Willow Acres","39"],["Pinewood Meadow","05"],["Broken Cart Orchard","31"],["Rattlesnake Ranch","47"],["Shadow Ridge Vineyard","96"],["Peach Tree Range","50"],["Elysian Orchard","19"],["Crown Meadow Fields","63"],["Mooseridge Meadow","40"],["Eastwood Orchard","90"],["Poison Ivy Gardens","48"],["Crescent Canyon Ranch","36"],["Cranberry Estate","41"],["Lone Pine Farm","49"],["Whispering Willow Range","82"],["Spring Blossom Ranch","66"],["Hollow Point Estate","27"],["Little Acorn Meadow","20"],["New Morning Pastures","79"],["Oak Wood Ranch","53"],["Crescent Canyon Orchard","56"],["Grassy Knoll Fields","07"],["Moonlight Gardens","73"],["Lucky Paws Estate","08"],["Crooked Creek Nursery","21"],["Small World Pastures","51"],["Crooked Creek Acres","45"],["Red Mountain Farmstead","78"],["Blueberry Basket Farms","60"],["Maple Springs Acres","54"],["Lucky Paws Farmstead","84"],["Wild Horse Pastures","55"]];
+
+console.log (offers[0][0])
+console.log (offers[1][0])
+console.log (offers[0][1])
+console.log (offers[1][1])
+
+let li =document.createElement("li")
+li.append(offers[0][0])
+document.body.append(li)
+
+offers=[["Crossroad Gardens","88"],["Strawberry Mountein Lands","57"]]
---
+++ tunnitoo5.js
@@ -0,0 +1,17 @@
+let arr = [5,8,4,2,3,1];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > 4) {
+    console.log(arr[i])
+  } else {
+    arr[i]++
+  }
+}
+
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 4) {
+      console.log(arr[i])
+    } else {
+      arr[i]++
+    }
+  }
