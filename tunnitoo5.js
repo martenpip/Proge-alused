f11056893ac141ab238aed73b6bb89aeefea2e30
martenpip <89940567+martenpip@users.noreply.github.com>
2021-10-01 10:12:38 +0300

    moist

--- tunnitoo5.js
+++ tunnitoo5.js
@@ -8,10 +8,21 @@ for (let i = 0; i < arr.length; i++) {
 }
 
 
+
 for (let i = 0; i < arr.length; i++) {
     if (arr[i] > 4) {
       console.log(arr[i])
     } else {
       arr[i]++
     }
+  }
+
+
+
+  let arr = [5,8,4,2,3,1];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 4)
+      console.log(arr[i]+" on suurem kui 4")
+    else
+      console.log(arr[i]+" on 4 või väiksem")
   }
