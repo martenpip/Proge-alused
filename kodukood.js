bf08858a403648e0f66be500b98b75a834540316
martenpip <89940567+martenpip@users.noreply.github.com>
2021-10-07 16:19:34 +0300

    ...

--- kodukood.js
+++ kodukood.js
@@ -1,6 +1,10 @@
+Math.random () *10;{
+
+}
+
 let sign = prompt("Sisesta üks number 1-10-ni");
 //let sign = prompt teeb nii et sonum tuleks ette 
-if (sign.toLowerCase() == "...") {
-  alert("...");
+if (sign.toLowerCase() == Math.random() *10) {
+  alert("Yay! Arvasid õige numbri ära!");
 }
-Math.random () *10
+ 
