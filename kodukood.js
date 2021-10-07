f087dc1ab635aee64d0be5bae202d6028cc86d3c
martenpip <89940567+martenpip@users.noreply.github.com>
2021-10-07 16:25:32 +0300

    yeah

--- kodukood.js
+++ kodukood.js
@@ -4,7 +4,7 @@ Math.random () *10;{
 
 let sign = prompt("Sisesta üks number 1-10-ni");
 //let sign = prompt teeb nii et sonum tuleks ette 
-if (sign.toLowerCase() == Math.random() *10) {
+if (sign.toLowerCase() == Math.floor(Math.random() * 10) + 1) {
   alert("Yay! Arvasid õige numbri ära!");
 }
- 
+
