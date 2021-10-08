03eabcf015010beb7470542d0b1e70d40cb0c2a4
martenpip <89940567+martenpip@users.noreply.github.com>
2021-10-08 08:58:49 +0300

    kood

--- kodukood.js
+++ kodukood.js
@@ -1,20 +1,20 @@
 
 asi = Math.floor(Math.random() * 10) + 1;
-
+console.log(asi);
 let sign = prompt("Sisesta üks number 1-10-ni");
 
 if (sign.toLowerCase() == Math.floor(Math.random() * 10) + 1) {
-  alert("Yay! Arvasid õige numbri ära!");
+
 
 
 
 }
 {
 
-if (asi < Math.floor(Math.random() * 10) + 1) 
+if (asi == sign) 
     alert("Yay! Arvasid õige numbri ära!");
 else
-    alert("Sorry, X ei ole õige number.")
+    alert("Sorry, X ei ole õige number.");
 }
 
 /*
