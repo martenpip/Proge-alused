690b182d2340b6abbfc460973aa4679a158c90e5
martenpip <89940567+martenpip@users.noreply.github.com>
2021-10-15 10:22:51 +0300

    yz

---
+++ tunnitoo6.js
@@ -0,0 +1,28 @@
+  function balanced(tahed) {
+    let str = "xxxyyy"
+    for (let taht of str) {
+    let x = 0
+        if(taht == "x")
+        x++;
+        if(taht == "y")
+        y++;  
+        }
+        return x==y
+    }
+
+    balanced ("!wac?_22d_$$")
+
+
+    function balanced_bonus(str) {
+    let foundletters = {};
+    for (let taht of taheed) {
+        if(taht == "x")
+        x++;
+        if(taht == "y")
+        y++;  
+    }
+    return false;
+}
+
+)e2ö.{cä5hbõkfkd@äe%5af/{#%94õfe}üc}j/ähö#ha/g-{bj2-g-)ü47.47a9idib)d9@2ö5}j.c%õ_i7üg@#k
+
