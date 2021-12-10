735ae14e7294b6c5eb4d125e78ea4220f647b5e4
martenpip <89940567+martenpip@users.noreply.github.com>
2021-12-10 09:20:33 +0000

    Update orderSummary.js

--- homework/orderSummary.js
+++ homework/orderSummary.js
@@ -1,8 +1,14 @@
-console.log("Order number", tellimus.orderNumber);
-console.log("Client name", tellimus.clientName);
+console.log("orderNumber", tellimus.orderNumber);
+console.log("clientName", tellimus.clientName);
+
 
 let sum = 0;
 tellimus.rows.forEach((row) => {
    sum += row.price * row.amount;
 });
-console.log("Sum of products", sum);
+console.log("Kogusumma:", sum);
+
+console.log ("Kogusumma käibemaks:", sum - sum /1.2)
+
+for(let toode of tellimus.rows)
+    console.log(toode.name)
